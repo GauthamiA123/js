a1d5bbb8cea6f537d44175e40b56c623f4b82896
GauthamiA123 <55610891+GauthamiA123@users.noreply.github.com>
2024-03-26 08:22:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1234
+let accountEmail = "gauthami.gmail.com"
+var accountPassword= "ashi"
+accountCity ="jaipur" /*we can declare like this as well*/
+// accountId = 1 (not allowed to change the id)
+let accountState; 
+console.log(accountId)
+
+accountEmail="gau@123"
+accountPassword="212121"
+accountCity="banglore"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*do not use var because it has functional scope  problem*/
+
